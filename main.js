4ac1b3826297551ef184c562fe26d2c981f15489
phelpsdevelops <raquanp78@icloud.com>
2026-03-28 01:50:41 -0400

    switched from html onclick to javascript event listener

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+document.querySelector("button").addEventListener('click',retrieve)
 function retrieve(){
    let task= document.querySelector('#enterText').value;
    document.querySelector('ol').innerHTML += `<li> ${task}</li>`
